147fff65e983219729d597fa8f9c29b9570cc3b5
Timo Clasen <goebeltimo@gmail.com>
2021-08-30 19:48:13 +0200

    Add Reset to Box Component

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,15 +1,22 @@
-import type { ElementType, ReactNode } from 'react';
+import { composeStyles } from '@vanilla-extract/css';
+import type { AllHTMLAttributes, ElementType, ReactNode } from 'react';
 import React, { forwardRef } from 'react';
+import * as resetStyles from '../reset.css';
 import type { Atoms } from '../../sprinkles.css';
 import { atoms } from '../../sprinkles.css';
 
-interface Props extends Atoms {
+export interface BoxProps
+    extends Omit<
+            AllHTMLAttributes<HTMLElement>,
+            'as' | 'width' | 'height' | 'color'
+        >,
+        Atoms {
     children: ReactNode;
     as?: ElementType;
     className?: string;
 }
 
-export const Box = forwardRef<HTMLElement, Props>(function Box(
+export const Box = forwardRef<HTMLElement, BoxProps>(function Box(
     { children, as: Element = 'div', className, ...props },
     ref
 ) {
@@ -24,13 +31,17 @@ export const Box = forwardRef<HTMLElement, Props>(function Box(
         }
     }
 
-    const atomicClasses = atoms(atomProps);
+    const styles = composeStyles(
+        resetStyles.base,
+        resetStyles.element[Element as keyof typeof resetStyles.element] || '',
+        atoms(atomProps)
+    );
 
     return (
         <Element
-            className={`${atomicClasses}${className ? ` ${className}` : ''}`}
-            {...nativeProps}
-            ref={ref}>
+            ref={ref}
+            className={`${styles}${className ? ` ${className}` : ''}`}
+            {...nativeProps}>
             {children}
         </Element>
     );
