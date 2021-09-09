34599e51a260b13516527e2dc76b75f18ae07b54
Timo Clasen <goebeltimo@gmail.com>
2021-09-09 19:43:03 +0200

    Add Inline Component

---
+++ src/components/Inline/Inline.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import type { ReactNode } from 'react';
+
+import type { BoxProps } from '../Box';
+import { Stack } from '../Stack';
+
+interface Props {
+    children: ReactNode;
+    as?: 'div' | 'ul' | 'ol';
+    space?: BoxProps['gap'];
+    wrap?: boolean;
+}
+
+export function Inline({
+    children,
+    as = 'div',
+    space = 'sm',
+    wrap,
+    ...props
+}: Props) {
+    return (
+        <Stack
+            as={as}
+            direction="horizontal"
+            space={space}
+            wrap={wrap}
+            {...props}>
+            {children}
+        </Stack>
+    );
+}
