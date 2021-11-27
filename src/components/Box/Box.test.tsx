73ee89c28b243b73fbbc7151b3afe4ff4ccf8ace
Timo Clasen <goebeltimo@gmail.com>
2021-11-27 19:43:40 +0100

    Add Color Mode Context Test

--- src/components/Box/Box.test.tsx
+++ src/components/Box/Box.test.tsx
@@ -8,4 +8,13 @@ describe('<Box />', () => {
     render(<Box>test</Box>);
     expect(screen.getByText('test')).toBeInTheDocument();
   });
+
+  it('renders with color mode context', () => {
+    render(
+      <Box contextualBackground="highlight">
+        <Box contextualColor="primary">test</Box>
+      </Box>
+    );
+    expect(screen.getByText('test')).toBeInTheDocument();
+  });
 });
