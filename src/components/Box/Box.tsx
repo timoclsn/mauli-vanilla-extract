421906aa03e1fe27307106383cd3fb7f967f60db
Timo Clasen <goebeltimo@gmail.com>
2022-01-21 14:19:07 +0100

    Refactor to Unnamed Arrow Function

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -27,61 +27,63 @@ export interface BoxProps
   fontSize?: keyof typeof fontSizes;
 }
 
-export const Box = forwardRef<HTMLElement, BoxProps>(function Box(
-  {
-    as: Element = 'div',
-    className,
-    color,
-    background,
-    contextualColor,
-    contextualBackground,
-    fontFamily,
-    fontSize,
-    fontWeight,
-    position,
-    ...props
-  },
-  ref
-) {
-  const atomProps: Record<string, unknown> = {};
-  const nativeProps: Record<string, unknown> = {};
+export const Box = forwardRef<HTMLElement, BoxProps>(
+  (
+    {
+      as: Element = 'div',
+      className,
+      color,
+      background,
+      contextualColor,
+      contextualBackground,
+      fontFamily,
+      fontSize,
+      fontWeight,
+      position,
+      ...props
+    },
+    ref
+  ) => {
+    const atomProps: Record<string, unknown> = {};
+    const nativeProps: Record<string, unknown> = {};
 
-  for (const key in props) {
-    if (sprinkles.properties.has(key as keyof Sprinkles)) {
-      atomProps[key] = props[key as keyof typeof props];
-    } else {
-      nativeProps[key] = props[key as keyof typeof props];
+    for (const key in props) {
+      if (sprinkles.properties.has(key as keyof Sprinkles)) {
+        atomProps[key] = props[key as keyof typeof props];
+      } else {
+        nativeProps[key] = props[key as keyof typeof props];
+      }
     }
+
+    const { foregroundColors, backgroundColors } = useColorMode();
+    const contextualColorValue = foregroundColors[contextualColor!];
+    const contextualBackgroundValue = backgroundColors[contextualBackground!];
+
+    const styles = clsx(
+      resetStyles.base,
+      resetStyles.element[Element as keyof typeof resetStyles.element],
+      sprinkles({
+        ...atomProps,
+        position: position || 'relative',
+        color: contextualColorValue ? contextualColorValue : color,
+        background: contextualBackgroundValue
+          ? contextualBackgroundValue.color
+          : background,
+      }),
+      fontFamily && fontFamilies[fontFamily],
+      fontWeight && fontWeights[fontWeight],
+      fontSize && fontSizes[fontSize].trimmed,
+      className
+    );
+
+    const element = <Element ref={ref} className={styles} {...nativeProps} />;
+
+    return contextualBackground ? (
+      <BackgroundProvider color={contextualBackground}>
+        {element}
+      </BackgroundProvider>
+    ) : (
+      element
+    );
   }
-
-  const { foregroundColors, backgroundColors } = useColorMode();
-  const contextualColorValue = foregroundColors[contextualColor!];
-  const contextualBackgroundValue = backgroundColors[contextualBackground!];
-
-  const styles = clsx(
-    resetStyles.base,
-    resetStyles.element[Element as keyof typeof resetStyles.element],
-    sprinkles({
-      ...atomProps,
-      position: position || 'relative',
-      color: contextualColorValue ? contextualColorValue : color,
-      background: contextualBackgroundValue
-        ? contextualBackgroundValue.color
-        : background,
-    }),
-    fontFamily && fontFamilies[fontFamily],
-    fontWeight && fontWeights[fontWeight],
-    fontSize && fontSizes[fontSize].trimmed,
-    className
-  );
-
-  const element = <Element ref={ref} className={styles} {...nativeProps} />;
-
-  return contextualBackground ? (
-    <BackgroundProvider color={contextualBackground}>
-      {element}
-    </BackgroundProvider>
-  ) : (
-    element
-  );
-});
+);
